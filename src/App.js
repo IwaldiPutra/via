1f868246c0a59d86acfc30f7df2dd3ee1a3a5559
G37dev <g37dev@gmail.com>
2025-04-11 21:53:26 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Ballpit from "./Ballpit";
+import Book from "./Book";
+import GradientText from "./GradientText";
+import flag from "./flag.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="page">
+      <div className="ball">
+        <Ballpit
+          count={50}
+          gravity={0.6}
+          friction={1}
+          wallBounce={0.9}
+          followCursor={false}
+          colors={["#6c9dc6", "black", "#6c9dc6"]}
+          lightIntensity={10}
+          maxSize={0.9}
+          minSize={0.4}
+        />
+      </div>
+
+      <div className="container ">
+        <img
+          src={flag}
+          className="w-100 rounded d-flex mx-auto"
+          style={{ maxWidth: "600px", height: "auto" }}
+        />
+        <br />
+        <GradientText
+          colors={["#52b1ff", "#0e76cb", "#52b1ff", "#0e76cb"]}
+          animationSpeed={4}
+          showBorder={false}
+          className="textTitle"
         >
-          Learn React
-        </a>
-      </header>
+          Happy Birthday{" "}
+        </GradientText>
+        <GradientText
+          colors={["#52b1ff", "#0e76cb", "#52b1ff", "#0e76cb"]}
+          animationSpeed={4}
+          showBorder={false}
+          className="textTitle2"
+        >
+          27 TH{" "}
+        </GradientText>
+        <br />
+        <br />
+        <Book />
+      </div>
     </div>
   );
 }
---
+++ src/Book.js
@@ -0,0 +1,77 @@
+import Lottie from "lottie-react";
+import animation from "./animation.json";
+import img1 from "./1.png";
+import img2 from "./2.png";
+
+function Book() {
+  return (
+    <div>
+      <div className="book-container">
+        <div className="book">
+          <input type="checkbox" name="page1" id="page1" />
+          <input type="checkbox" name="page2" id="page2" />
+          <input type="checkbox" name="page3" id="page3" />
+          <input type="checkbox" name="page4" id="page4" />
+          <div className="pages">
+            <div className="page1">
+              <label htmlFor="page1">
+                <div className="fade-target">
+                  <div className="" style={{ height: "180px" }}>
+                    <Lottie
+                      className="d-flex justify-content-center align-items-center m-auto h-100"
+                      animationData={animation}
+                      loop={true}
+                    />
+                    <p className="text-white mt-2">Click Me :{")"}</p>
+                  </div>{" "}
+                </div>
+              </label>
+            </div>
+            <div className="page2">
+              <label htmlFor="page2">
+                <div className="fade-target">
+                  <div className="imgbd rounded">
+                    <img
+                      src={img1}
+                      className="w-100 rounded "
+                      style={{ maxWidth: "145px", height: "auto" }}
+                    />
+                  </div>
+                  <div
+                    className="text-white fw-bold text-center mb-0 mt-3 px-1"
+                    style={{ lineHeight: "16px", fontSize: "12px" }}
+                  >
+                    I hope that in the future you will not only be one year
+                    older but also become wiser
+                  </div>
+                </div>
+              </label>
+            </div>
+            <div className="page3">
+              <label htmlFor="page3">
+                <div className="fade-target">
+                  <div className="imgbd rounded">
+                    <img
+                      src={img2}
+                      className="w-100 rounded "
+                      style={{ maxWidth: "145px", height: "auto" }}
+                    />
+                  </div>
+
+                  <div
+                    className="text-white fw-bold text-center mb-0 mt-3 px-1"
+                    style={{ lineHeight: "16px", fontSize: "12px" }}
+                  >
+                    May in this new age, all your dreams come true one by one.{" "}
+                  </div>
+                </div>
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Book;
